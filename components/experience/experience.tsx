3a37a03de9c15fdb799dc7c350bc5828e2770b33
Apostolos Karamanis <127772424+qaramanis@users.noreply.github.com>
2025-07-26 22:13:23 +0300

    minor fix

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import TitleCard from "../title-card";
+
+const experience = [
+  {
+    title: "Freelancer",
+    description: "Anything web related",
+    date: "2024 - now",
+  },
+];
+
 export default function Experience() {
-  return <div></div>;
+  return (
+    <div className="pt-[4rem]">
+      {experience.map((item, index) => (
+        <div key={index}>
+          <TitleCard
+            title={item.title}
+            description={item.description}
+            date={item.date}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
--- components/title-card.tsx
+++ components/title-card.tsx
@@ -10,14 +10,14 @@ export default function TitleCard({
   date,
 }: TitleCardProps) {
   return (
-    <div className="bg-background rounded-lg shadow-md">
+    <div className="bg-background">
       <div className="pt-[1rem] flex flex-col justify-between border-b border-[#666666] hover:border-foreground relative overflow-hidden group transition-all duration-500 ease-out">
         <div className="flex flex-row justify-between">
           <div>{title}</div>
           <div className="text-[#555555]">{date}</div>
         </div>
         <p className="text-[#555555] truncate">{description}</p>
-        <div className="absolute bottom-0 left-0 h-[1px] bg-foreground w-0 group-hover:w-full transition-all duration-500 ease-in-out" />
+        <div className="absolute bottom-0 left-0 h-[0.75px] bg-foreground w-0 group-hover:w-full transition-all duration-500 ease-in-out" />
       </div>
     </div>
   );
