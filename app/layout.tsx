18e6a05312aa80d084d397739eb1492efecfa388
Apostolos Karamanis <apostkaram@gmail.com>
2025-08-07 12:04:49 +0300

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Apostolos Karamanis",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <Analytics />
+        {children}
+      </body>
     </html>
   );
 }
