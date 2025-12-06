fae8d9158e863cf554b248ea68017abf65e47ec7
qaramanis <127772424+qaramanis@users.noreply.github.com>
2025-12-06 09:31:54 +0200

    Update works.ts

--- data/works.ts
+++ data/works.ts
@@ -26,46 +26,4 @@ export const works: Work[] = [
     categories: [""],
     imageUrl: "/works/images/vidavo-nda.png",
   },
-  {
-    id: 3,
-    title: "Project -",
-    description: "",
-    categories: [""],
-  },
-  {
-    id: 4,
-    title: "Project -",
-    description: "",
-    categories: [""],
-  },
-  {
-    id: 5,
-    title: "Project -",
-    description: "",
-    categories: [""],
-  },
-  {
-    id: 6,
-    title: "Project -",
-    description: "",
-    categories: [""],
-  },
-  {
-    id: 7,
-    title: "Project -",
-    description: "",
-    categories: [""],
-  },
-  {
-    id: 8,
-    title: "Project -",
-    description: "",
-    categories: [""],
-  },
-  {
-    id: 9,
-    title: "Project -",
-    description: "",
-    categories: [""],
-  },
 ];
